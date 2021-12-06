606ce073cfbba8b6eca4aa71db24fb9b9d3a4d03
takahashi <skeletonking812@gmail.com>
2021-12-06 14:49:36 +0200

    added refreshAPI button

--- script.js
+++ script.js
@@ -3,25 +3,62 @@ let filteredCoinsData;
 let currentPage = 1
 let coinsPerPage = 100
 let previousSearch;
-
-$(".fetchBtn").click(() => {
-    fetchData()
-})
+let lastRefreshedApiDate;
 
 function fetchData() {
     // const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&page=1&per_page=10"
     const url = "https://api.coingecko.com/api/v3/coins/list"
+    // insert loading screen after search container
+
+    if ($(".search-and-fetchBtn-container").length) { // if element exists:
+        $(".cardsContainer").remove()
+        $(".pagination").remove()
+        $("main").append(`<img class="loading" src="./img/loading.gif" alt="loadingIMG">`)
+        console.log("added loading image")
+    }
+
     $.get(url, (data) => {
         coinsData = data
-        filteredCoinsData = coinsData
         console.log(coinsData)
-        clearMain()
-        createSearchBox()
-        renderCards()
-        createPagination()
+        filteredCoinsData = coinsData
+        lastRefreshedApiDate = new Date();
+        if ($(".search-and-fetchBtn-container").length) { // if element exists:
+            $(".loading").remove()
+            renderCards()
+            createPagination()
+            previousSearch = ""
+            $(".search-btn").trigger("click") // keep filter on refresh
+        } else {
+            clearMain()
+            createSearchBox()
+            renderCards()
+            createPagination()
+        }
     })
 }
 
+function timePassedSinceFetch(miliseconds) {
+    let tooltip = $("#refreshApiBtn .tooltip")
+    msPassed = new Date(miliseconds)
+    if (msPassed < 1000) {
+        tooltip.text("Last refreshed: now.")
+        return;
+    }
+    // 24 hours = 86400000 miliseconds
+    if (msPassed < 86400000) {
+        if (msPassed.getUTCHours() == 0) {
+            if (msPassed.getMinutes() == 0) {
+                tooltip.text(`Last refreshed: ${msPassed.getUTCSeconds()} sec ago.`)
+            } else {
+                tooltip.text(`Last refreshed: ${msPassed.getUTCMinutes()} min ago.`)
+            }
+        } else {
+            tooltip.text(`Last refreshed: ${msPassed.getUTCHours()} hours ago.`)
+        }
+    } else {
+        tooltip.text("Last refreshed: over 24 hours ago.")
+    }
+}
 
 function createPagination() {
     const totalPages = Math.ceil(filteredCoinsData.length / coinsPerPage)    
@@ -59,46 +96,46 @@ function pageChange(e) {
     currentPage = e.target.innerText
     $(".cardsContainer").remove()
     $(".pagination").remove()
-    console.log("pageChange: renderCards()")
     renderCards()
-    console.log("pageChange: createPagination()")
     createPagination()
-    console.log("pageChange: finito")
 }
 
 function clearMain() {
     $("main").html("")
 }
 
-// #region || search + mic
+// #region || search + mic + api-button
 
 function createSearchBox() {
     $("main").append(`
-    <div class="search-container">
-        <input type="text" placeholder="Search by symbol..." class="search-text-input">
-        <button class="search-btn">
-            <i class="fa fa-search"></i>
-        </button>
-        <button class="search-mic-btn">
-            <i class="fa fa-microphone"></i>
+    <div class="search-and-fetchBtn-container">
+        <div class="search-container">
+            <input type="text" placeholder="Search by symbol..." class="search-text-input">
+            <button class="search-btn">
+                <i class="fa fa-search"></i>
+            </button>
+            <button class="search-mic-btn">
+                <i class="fa fa-microphone"></i>
+            </button>
+        </div>
+        <button id="refreshApiBtn" onclick="fetchData()" onmouseover="timePassedSinceFetch(Math.abs((new Date()) - lastRefreshedApiDate))">
+            <i class="fa fa-refresh"></i>
+            <span class="tooltip">Last refreshed: </span>
         </button>
     </div>
     `)
-    const searchBtn = document.querySelector(".search-btn")
-    const searchMicBtn = document.querySelector(".search-mic-btn")
-    const searchTextInput = document.querySelector(".search-text-input")
     
     // #region || search filter
 
-    searchBtn.addEventListener("click", searchFilterClick)
+    $(".search-btn").click(searchFilterClick)
 
     function searchFilterClick() {
-        if (previousSearch == searchTextInput.value) {
+        if (previousSearch == $(".search-text-input").val()) {
             return
         }
-        previousSearch = searchTextInput.value
+        previousSearch = $(".search-text-input").val()
         filteredCoinsData = coinsData.filter(each => {
-            return each.symbol.toLowerCase().includes(searchTextInput.value.toLowerCase())
+            return each.symbol.toLowerCase().includes($(".search-text-input").val().toLowerCase())
         })
         currentPage = 1
         if (!filteredCoinsData.length) {
@@ -124,14 +161,14 @@ function createSearchBox() {
         recognition.interimResults = true
         recognition.lang = "en-US"
         
-        searchMicBtn.addEventListener("click", e => {
+        $(".search-mic-btn").click(e => {
             console.log("listening...")
             recognition.start()
-            searchMicBtn.classList.add("listening")
+            $(".search-mic-btn").addClass("listening")
         })
         
         recognition.onresult = function(e) {
-            searchTextInput.value = e.results[0][0].transcript // includes its thinking process
+            $(".search-text-input").val(e.results[0][0].transcript) // includes its thinking process
             // for final check for .isFinal == true
             console.log("finished interim")
             if (e.results[0].isFinal == true) {
@@ -143,34 +180,34 @@ function createSearchBox() {
         recognition.onspeechend = function() {
             recognition.stop()
             console.log("stopped listening: speech ended.")
-            searchMicBtn.blur()
-            searchMicBtn.classList.remove("listening")
+            $(".search-mic-btn").blur()
+            $(".search-mic-btn").removeClass("listening")
         }
         
         recognition.onerror = function(event) {
             console.log('Stopped listening due to error: ' + event.error)
             recognition.stop()
-            searchMicBtn.blur()
-            searchMicBtn.classList.remove("listening")
+            $(".search-mic-btn").blur()
+            $(".search-mic-btn").removeClass("listening")
         }
     } catch (error) {
         console.warn("Microphone was removed due to lack of browser support. ERR:", error)
-        searchMicBtn.remove()
+        $(".search-mic-btn").remove()
     }
     
-
-
     // #endregion
 }
 
+// #endregion
+// #region || execution
+fetchData()
 // #endregion
 
 /*
 
 todo;
-button sliders
-refresh API button
 about page
 what is 2nd page?
 
 */
+
